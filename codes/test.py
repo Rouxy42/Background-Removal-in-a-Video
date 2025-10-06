36515d0001728bf53428f4dccae6d4f78165c323
Rouxy42 <ilario.franciamore24@gmail.com>
2025-10-06 12:24:56 +0200

    update test.py update BackgroundRemoval update Updates

--- codes/test.py
+++ codes/test.py
@@ -15,6 +15,7 @@ data = loadmat(file_path)
 X = np.array(data["X"])  # converti en ndarray pour sécurité
 m = np.array(data["m"])
 n = np.array(data["n"])
+print(X)
 
 #Ici, on a récupéré la big matrice avec les images vectorisées (X c'est la vidéo)
 #m et n sont les dimensions (en pixel) des images
@@ -22,12 +23,21 @@ n = np.array(data["n"])
 # Données
 m = int(m)
 n = int(n)
-taille = m * n  # Taille du vecteur image
-
+p = m * n  # Taille du vecteur image
+t = 100
 
 
 # Objectif
 c = [1, -1, -1]
 
-# Contraintes
-A = [[1, -1, -1, 0], [1, -1, 0, 1]]
+# Création de la matrice contraintes A 
+A = np.zeros((2*t, 3*t +1))
+for j in range(t):
+    # Première contrainte pour les 100 itérations
+    A[j, 0] = 1
+    A[j, j + 1] = 1
+    A[j, j + 101] = -1
+    # Deuxième contrainte pour les 100 itérations
+    A[100 + j, 0] = -1
+    A[100 + j, j + 1] = 1
+    A[100 + j, j + 201] = -1
