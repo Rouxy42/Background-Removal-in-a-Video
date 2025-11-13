c72f652705e9460ea79e809ed388ee4bb0d86bea
kypia <fleury.cyril0205@icloud.com>
2025-11-13 17:50:29 +0100

    Update

--- codes/problem_solver.py
+++ codes/problem_solver.py
@@ -4,13 +4,7 @@ from scipy.optimize import linprog
 def one_problem_solve(j, c, A2_sparse, bounds, X):
     print(f"----- Début du calcul {j+1} -----")
     b_eq = np.concatenate([X[:, j], -X[:, j]])
-    model2 = linprog(
-        c = c,
-        A_eq=A2_sparse,
-        b_eq=b_eq,
-        bounds=bounds,
-        method='highs-ipm'
-    )
+    model2 = linprog(c=c, A_eq=A2_sparse, b_eq=b_eq, bounds=bounds, method='highs-ipm')
 
     if model2.success:
         return model2.x[0]
